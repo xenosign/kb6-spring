504bde95e3a9df5612f42ee7be1d1de943ff12cd
Tetz <xenosign@naver.com>
2025-06-11 16:37:24 +0900

    250611 실습 진행

---
+++ src/test/java/JDBCTest.java
@@ -0,0 +1,30 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Slf4j
+public class JDBCTest {
+    @BeforeAll
+    public static void setup() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("JDBC 드라이버 연결이 된다")
+    public void testConnection() {
+        String url = "jdbc:mysql://localhost:3306/tetzdb";
+        try (Connection con = DriverManager.getConnection(url, "root", "1234")) {
+            log.info("con = {}", con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
